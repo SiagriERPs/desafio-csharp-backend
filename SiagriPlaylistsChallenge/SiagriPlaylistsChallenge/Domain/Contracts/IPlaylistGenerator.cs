ead00cd5222da4715388a0f9699f4378fc45f717
victorstevan <victorstevan@discente.ufg.br>
2021-07-19 23:21:43 -0300

    feat: add contract interface for generating playlist

---
+++ SiagriPlaylistsChallenge/SiagriPlaylistsChallenge/Domain/Contracts/IPlaylistGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiagriPlaylistsChallenge.Domain.Contracts
+{
+    /// <summary>
+    /// Made this playlist generator contract in a very generic way 
+    /// that support domain logic change in the client specifications 
+    /// just in case a crazy manager decides to change the service specifications :P
+    /// </summary>
+    /// <typeparam name="T">Playlist output type</typeparam>
+    /// <typeparam name="V">Playlist `value` input parameter for generating the musics</typeparam>
+    public interface IPlaylistGenerator<T, V>
+    {
+        T GetPlaylistMusics(V parameter);
+    }
+}
