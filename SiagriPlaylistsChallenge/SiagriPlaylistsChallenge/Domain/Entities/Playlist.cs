afb2ed7879ea1336fbaaf3044c1e6ebffbb4d6ad
victorstevan <victorstevan@discente.ufg.br>
2021-07-19 23:31:01 -0300

    feat: add playlist entity scaffold

---
+++ SiagriPlaylistsChallenge/SiagriPlaylistsChallenge/Domain/Entities/Playlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiagriPlaylistsChallenge.Domain.Entities
+{
+    public class Playlist
+    {
+        public Guid Id { get; set; }
+        public List<string> Musics { get; set; }
+
+        public string City { get; set; }
+
+
+
+        public enum PlaylistState
+        {
+            Creating,
+            Ready
+        }
+    }
+}
